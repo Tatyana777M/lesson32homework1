94b0cd740e43629d871ae17f9b8560dcc1b12950
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-12 17:06:02 +0100

    Class Shoe has finished

---
+++ src/Shoe.java
@@ -0,0 +1,49 @@
+public class Shoe {
+    public String name;
+    public double price;
+    public double size;
+    public double rating;
+    private int stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Shoe{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", size=" + size +
+                ", rating=" + rating +
+                ", stock=" + stock +
+                '}';
+    }
+
+    public Shoe(String name, double price, double size, double rating, int stock) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+        this.rating = rating;
+        this.stock = stock;
+
+
+    }
+}
+
