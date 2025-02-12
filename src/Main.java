79cd02c1889401b03253ff60265f3df3a585f140
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-12 21:24:15 +0100

    Class Main has finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
 
-    }
-}
+    public static void main(String[] args) {
+        List<Shoe> shoes = new ArrayList<>();
+        shoes.add(new Shoe("Nike Air Max", 90.0, 39, 4.5, 10));
+        shoes.add(new Shoe("Adidas Boost", 120.0, 38, 4.7, 15));
+        shoes.add(new Shoe("Puma Suede", 80.0, 37, 4.3, 20));
+        shoes.add(new Shoe("Reebok Dance", 95.0, 40, 4.0, 5));
+
+        Collections.sort(shoes, new PriceComparator());
+        System.out.println("Sort by price:");
+        for (Shoe shoe : shoes) {
+            System.out.println(shoe.getName() + " - " + shoe.getPrice());
+        }
+
+        Collections.sort(shoes, new SizeComparator());
+        System.out.println("\nSort by size:");
+        for (Shoe shoe : shoes) {
+            System.out.println(shoe.getName() + " - " + shoe.getSize());
+        }
+
+        Collections.sort(shoes, new NameComparator());
+        System.out.println("\nSort by name:");
+        for (Shoe shoe : shoes) {
+            System.out.println(shoe.getName() + " - " + shoe.getRating());
+        }
+
+        Collections.sort(shoes, new ComparatorShoeName());
+        System.out.println("Sorted by name:");
+        for (Shoe shoe : shoes) {
+            System.out.println(shoe);
+        }
+    }}
+
+
+
+
